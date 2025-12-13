6f23ee58f7886252762610e2ed6213d800758d26
Bhavin Raut <Bhavin.Raut@teknopoint.local>
2025-12-13 14:58:28 +0530

    super rely branch

---
+++ s2.js
@@ -0,0 +1,17 @@
+// server.js - Your Private Relay Node
+const http = require('http');
+const Gun = require('gun');
+
+const server = http.createServer(function(req, res) {
+    if(Gun.serve(req, res)){ return } // Handle Gun requests
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Relay is Active!');
+});
+
+const gun = Gun({ web: server });
+
+server.listen(8765, () => {
+    console.log('---------------------------------------');
+    console.log('✅ Relay is running at http://localhost:8765/gun');
+    console.log('---------------------------------------'); 
+});
